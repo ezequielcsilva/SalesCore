f33a9763874fe0f726a2d60fff85431849b3208a
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2024-09-23 20:18:36 -0300

    test: #20240923 update and delete order

--- tests/SalesCore.IntegrationTests/Application/Orders/OrdersTests.cs
+++ tests/SalesCore.IntegrationTests/Application/Orders/OrdersTests.cs
@@ -1,7 +1,9 @@
 ﻿using Bogus;
 using FluentAssertions;
 using SalesCore.Application.Orders.CreateOrder;
+using SalesCore.Application.Orders.DeleteOrder;
 using SalesCore.Application.Orders.GetOrderById;
+using SalesCore.Application.Orders.UpdateOrder;
 using SalesCore.Domain.Orders;
 
 namespace SalesCore.IntegrationTests.Application.Orders;
@@ -124,5 +126,100 @@ public class OrdersTests(IntegrationTestWebAppFactory factory) : BaseIntegration
         retrievedOrder.Items.ElementAt(0).Quantity.Should().Be(orderItems[0].Quantity);
         retrievedOrder.Items.ElementAt(1).ProductId.Should().Be(orderItems[1].ProductId);
         retrievedOrder.Items.ElementAt(1).Quantity.Should().Be(orderItems[1].Quantity);
+    }
+
+    [Fact]
+    public async Task Update_ShouldUpdateOrderItems()
+    {
+        // Arrange
+        var orderItems = new List<CreateOrderItemRequest>
+    {
+        new(_faker.Random.Guid(), 1, 100m),
+        new(_faker.Random.Guid(), 2, 150m)
+    };
+
+        var createCommand = new CreateOrderCommand(new CreateOrderRequest(
+            _faker.Random.Guid(),
+            _faker.Random.Guid(),
+            400m,
+            orderItems,
+            string.Empty,
+            false,
+            0m
+        ));
+
+        var createResult = await Sender.Send(createCommand);
+        createResult.IsSuccess.Should().BeTrue();
+
+        var createdOrder = DbContext.Set<Order>().FirstOrDefault(o => o.Id == createResult.Value.Id);
+        createdOrder.Should().NotBeNull();
+
+        var updatedOrderItems = new List<UpdateOrderItemRequest>
+    {
+        new(orderItems[0].ProductId, 3, 110m),
+        new(orderItems[1].ProductId, 4, 155m)
+    };
+
+        var updateCommand = new UpdateOrderCommand(new UpdateOrderRequest(
+            createdOrder!.Id,
+            updatedOrderItems
+        ));
+
+        // Act
+        var updateResult = await Sender.Send(updateCommand);
+
+        // Assert
+        updateResult.IsSuccess.Should().BeTrue();
+
+        var updatedOrder = DbContext.Set<Order>().FirstOrDefault(o => o.Id == createdOrder.Id);
+        updatedOrder.Should().NotBeNull();
+        updatedOrder?.OrderItems.Should().HaveCount(2);
+
+        var firstItem = updatedOrder!.OrderItems.ElementAt(0);
+        firstItem.ProductId.Should().Be(updatedOrderItems[0].ProductId);
+        firstItem.Quantity.Should().Be(updatedOrderItems[0].Quantity);
+        firstItem.Price.Should().Be(updatedOrderItems[0].Price);
+
+        var secondItem = updatedOrder.OrderItems.ElementAt(1);
+        secondItem.ProductId.Should().Be(updatedOrderItems[1].ProductId);
+        secondItem.Quantity.Should().Be(updatedOrderItems[1].Quantity);
+        secondItem.Price.Should().Be(updatedOrderItems[1].Price);
+    }
+
+    [Fact]
+    public async Task Delete_ShouldRemoveOrder()
+    {
+        // Arrange
+        var orderItems = new List<CreateOrderItemRequest>
+        {
+            new(_faker.Random.Guid(), 1, 100m),
+            new(_faker.Random.Guid(), 2, 150m)
+        };
+
+        var createCommand = new CreateOrderCommand(new CreateOrderRequest(
+            _faker.Random.Guid(),
+            _faker.Random.Guid(),
+            400m,
+            orderItems,
+            string.Empty,
+            false,
+            0m
+        ));
+
+        var createResult = await Sender.Send(createCommand);
+        createResult.IsSuccess.Should().BeTrue();
+
+        var createdOrder = DbContext.Set<Order>().FirstOrDefault(o => o.Id == createResult.Value.Id);
+        createdOrder.Should().NotBeNull();
+
+        // Act
+        var deleteCommand = new DeleteOrderCommand(createdOrder!.Id);
+        var deleteResult = await Sender.Send(deleteCommand);
+
+        // Assert
+        deleteResult.IsSuccess.Should().BeTrue();
+
+        var deletedOrder = DbContext.Set<Order>().FirstOrDefault(o => o.Id == createdOrder.Id);
+        deletedOrder.Should().BeNull();
     }
 }
