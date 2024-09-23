98f5ab5b5eec051660fc31b841045b486f13ec16
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2024-09-23 11:54:33 -0300

    feat: #20240923 register DI application

---
+++ src/SalesCore.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MediatR.NotificationPublishers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SalesCore.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+            configuration.NotificationPublisher = new ForeachAwaitPublisher();
+        });
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
+
+        return services;
+    }
+}
