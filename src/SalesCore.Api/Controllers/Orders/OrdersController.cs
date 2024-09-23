dfffcaba9b9d85dbcfc6ed90aad251546b56076a
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2024-09-23 12:44:54 -0300

    feat: #20240923 add routes crud order

--- src/SalesCore.Api/Controllers/Orders/OrdersController.cs
+++ src/SalesCore.Api/Controllers/Orders/OrdersController.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SalesCore.Application.Orders.CreateOrder;
+using SalesCore.Application.Orders.DeleteOrder;
 using SalesCore.Application.Orders.GetOrderById;
+using SalesCore.Application.Orders.UpdateOrder;
 
 namespace SalesCore.Api.Controllers.Orders;
 
@@ -18,5 +21,49 @@ public class OrdersController(ISender sender) : ControllerBase
         }
 
         return Ok(result.Value);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder([FromBody] CreateOrderRequest orderRequest, CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new CreateOrderCommand(orderRequest), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok();
+    }
+
+    [HttpPut("{orderId:guid}")]
+    public async Task<IActionResult> UpdateOrder([FromRoute] Guid orderId, [FromBody] UpdateOrderRequest orderRequest, CancellationToken cancellationToken)
+    {
+        if (orderRequest.OrderId != orderId)
+        {
+            return Forbid();
+        }
+
+        var result = await sender.Send(new UpdateOrderCommand(orderRequest), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok();
+    }
+
+    [HttpDelete("{orderId:guid}")]
+    public async Task<IActionResult> DeleteOrder([FromRoute] Guid orderId, CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new DeleteOrderCommand(orderId), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok();
     }
 }
