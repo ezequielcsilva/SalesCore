0b65d7c7b21ed558fad58f6fb8aecb3ea29ba070
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2024-09-24 15:52:25 -0300

    feat: #20240924 add orderItem validation

--- src/SalesCore.Application/Orders/CreateOrder/CreateOrderValidator.cs
+++ src/SalesCore.Application/Orders/CreateOrder/CreateOrderValidator.cs
@@ -17,5 +17,20 @@ internal sealed class CreateOrderValidator : AbstractValidator<CreateOrderComman
         RuleFor(c => c.Order.OrderItems.Count)
             .GreaterThan(0)
             .WithMessage("The order needs to have at least 1 item");
+
+        RuleForEach(c => c.Order.OrderItems).ChildRules(items =>
+        {
+            items.RuleFor(i => i.Quantity)
+                .GreaterThan(0)
+                .WithMessage("The quantity of each item must be greater than zero");
+
+            items.RuleFor(i => i.Price)
+                .GreaterThan(0)
+                .WithMessage("The price of each item must be greater than zero");
+        });
+
+        RuleFor(c => c.Order.Amount)
+            .Equal(c => c.Order.OrderItems.Sum(i => i.Quantity * i.Price))
+            .WithMessage("The total amount must be equal to the sum of quantity * price for each item.");
     }
 }
--- src/SalesCore.Application/Orders/UpdateOrder/UpdateOrderValidator.cs
+++ src/SalesCore.Application/Orders/UpdateOrder/UpdateOrderValidator.cs
@@ -9,5 +9,16 @@ internal sealed class UpdateOrderValidator : AbstractValidator<UpdateOrderComman
         RuleFor(c => c.Order.OrderItems.Count)
             .GreaterThan(0)
             .WithMessage("The order needs to have at least 1 item");
+
+        RuleForEach(c => c.Order.OrderItems).ChildRules(items =>
+        {
+            items.RuleFor(i => i.Quantity)
+                .GreaterThan(0)
+                .WithMessage("The quantity of each item must be greater than zero");
+
+            items.RuleFor(i => i.Price)
+                .GreaterThan(0)
+                .WithMessage("The price of each item must be greater than zero");
+        });
     }
 }
